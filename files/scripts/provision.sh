fbafa2994494a1109ae32edeaaae393010aae384
Brett Tasker <brett@silverstripe.com>
2021-08-31 15:33:30 +1200

    Only setup SSL and reload Nginx if it is installed and enabled

--- files/scripts/provision.sh
+++ files/scripts/provision.sh
@@ -14,9 +14,11 @@ fi
 
 /opt/muna/scripts/setup_ssenv "${NAMESPACE}"
 /opt/muna/scripts/setup_secrets "${NAMESPACE}"
-/opt/muna/scripts/setup_ssl "${NAMESPACE}"
-
 systemctl reload apache2
 
-# Only try to reload nginx if it is installed and running
-(systemctl is-active --quiet nginx && nginx -t && systemctl reload nginx) || true
+# Only try to setup SSL and reload nginx if it is installed and running
+if [ systemctl is-active --quiet nginx ]; then
+    # Install SSL Certificates and reload Nginx
+    /opt/muna/scripts/setup_ssl "${NAMESPACE}"
+    systemctl reload nginx
+fi
