7d7b82b3926cef414aa47b51c8239bf67cad1f26
Brett Tasker <brett.tasker@gmail.com>
2021-09-02 10:33:57 +1200

    Support parallel execution (#4)
    * Only setup SSL and reload Nginx if it is installed and enabled
    * Use vhost prefix for uniqid to allow for parallel usage across vhosts

--- files/scripts/provision.sh
+++ files/scripts/provision.sh
@@ -14,9 +14,11 @@ fi
 
 /opt/muna/scripts/setup_ssenv "${NAMESPACE}"
 /opt/muna/scripts/setup_secrets "${NAMESPACE}"
-/opt/muna/scripts/setup_ssl "${NAMESPACE}"
-
 systemctl reload apache2
 
-# Only try to reload nginx if it is installed and running
-(systemctl is-active --quiet nginx && nginx -t && systemctl reload nginx) || true
+# Only try to setup SSL and reload nginx if it is installed and running
+if [[ $(systemctl is-active --quiet nginx) -eq 0 ]]; then
+    # Install SSL Certificates and reload Nginx
+    /opt/muna/scripts/setup_ssl "${NAMESPACE}"
+    nginx -t && systemctl reload nginx
+fi
